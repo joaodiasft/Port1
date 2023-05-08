8aed5f1ac4b6dd47c9a61b4a25361dbdbb2e6bde
JoaoClaudio <102878752+joaodiasft@users.noreply.github.com>
2023-05-07 23:45:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".btn-mobile").click(function(){
+        $(".menu-mobile").slideToggle("slow")
+    })
+})
+
+//<script>
+//$(document).ready(function(){
+ //   $(".btn-mobile").click(function(){
+//        $(".menu-mobile").slideToggle("slow")
+ //   })
+//})
+//</script>
